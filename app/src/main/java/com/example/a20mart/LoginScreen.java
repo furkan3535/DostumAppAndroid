446ee058ee23c5a0efbc2aaa70a28ae44e3c7625
Furkan P. Acikgoz <furkanpolat.acikgoz@ibtech.com.tr>
2020-05-22 00:02:49 +0300

    Firebase Integration and Login Screen

---
+++ app/src/main/java/com/example/a20mart/LoginScreen.java
@@ -0,0 +1,70 @@
+package com.example.a20mart;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class LoginScreen extends AppCompatActivity {
+
+    EditText emailET,passwordET;
+    Button signInWithEmail;
+    private FirebaseAuth mAuth;
+
+    private void intentToMain(){
+        startActivity(new Intent(this,MainActivity.class));
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        emailET = (EditText)findViewById( R.id.emailET);
+        passwordET = (EditText)findViewById( R.id.passwordET);
+        signInWithEmail = (Button)findViewById(R.id.signInWithEmailBtn);
+        mAuth = FirebaseAuth.getInstance();
+        signInWithEmail.setOnClickListener(signInWithEmailPressed);
+    }
+
+
+
+    View.OnClickListener signInWithEmailPressed = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            mAuth.signInWithEmailAndPassword(emailET.getText().toString(), passwordET.getText().toString())
+                    .addOnCompleteListener( new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            String TAG = "FB Login";
+                            if (task.isSuccessful()) {
+                                // Sign in success, update UI with the signed-in user's information
+                                Log.d(TAG, "signInWithEmail:success");
+                                FirebaseUser user = mAuth.getCurrentUser();
+                                intentToMain();
+
+                            } else {
+                                // If sign in fails, display a message to the user.
+                                Log.w(TAG, "signInWithEmail:failure", task.getException());
+                                Toast.makeText(LoginScreen.this, "Authentication failed.",
+                                        Toast.LENGTH_SHORT).show();
+                            }
+
+                        }
+                    });
+
+        }
+    };
+}
+
